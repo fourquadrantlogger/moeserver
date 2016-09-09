acf46c2129fbbf25b16b793794a7d55b104c20e4
timeloveboy <734991033@qq.com>
2016-09-10 02:54:00 +0800

    ~

---
+++ src/utils/FileUtil.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class FileUtil {
+    public static String getContent(File file) {
+        String content = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                content += line + "\r\n";
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+}
